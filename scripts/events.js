71393dfe01b02fb76f37d10c2e8afedf626d9488
BeroMaze <Berning.corey@gmail.com>
2017-02-08 16:08:24 -0800

    add redirect

--- scripts/events.js
+++ scripts/events.js
@@ -1,3 +1,8 @@
+if (location.protocol != 'https:')
+{
+ location.href = 'https:' + window.location.href.substring(window.location.protocol.length);
+}
+
 var txt = ["Search For....","Queen Anne","Pioneer Square","Sushi", "Micro-Brew","Cocktail Bars","find me a BEER......", "GET ME DRUNK!!!"];
 var timeOut;
 var counter =0;
